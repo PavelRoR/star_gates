64914b44b66cd90743e04286ead61975ee12056c
pavelp21 <pavelvp2182@yandex.ru>
2018-09-26 22:43:45 +0500

    Desktop is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    /* Якорь */
+    $(function () {
+        $("a[href^='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
+    /* Таймер */
+    var clock;
+    var futureDate = new Date("September 28, 2018 00:00 AM UTC+3");
+    var currentDate = new Date();
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-
-
-/*Конец документа*/
+    function dayDiff(first, second) {
+        return (second - first) / (1000 * 60 * 60 * 24);
+    }
+    if (dayDiff(currentDate, futureDate) < 100) {
+        $('.clock').addClass('twoDayDigits');
+    } else {
+        $('.clock').addClass('threeDayDigits');
+    }
+    if (diff < 0) {
+        diff = 0;
+        $('#prices').css('padding', '40px 20px');
+        $('.price_full span').text('4 400руб.');
+        $('.button_buy').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2183&clean=true&lg=ru');
+        $('.timer').remove();
+    }
+    clock = $('.clock').FlipClock(diff, {
+        clockFace: 'HourlyCounter',
+        countdown: true,
+        language: 'ru',
+        callbacks: {
+            stop: function () {
+                $('#prices').css('padding', '40px 20px');
+                $('.price_full span').text('4 400руб.');
+                $('.button_buy').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2183&clean=true&lg=ru');
+                $('.timer').remove();
+            }
+        },
+    });
+    /*Страые браузеры*/
+    var yaBrowserUpdater = new ya.browserUpdater.init({
+        "lang": "ru",
+        "browsers": {
+            "yabrowser": "15.10",
+            "chrome": "52",
+            "ie": "10",
+            "opera": "40",
+            "safari": "8",
+            "fx": "47",
+            "iron": "35",
+            "flock": "Infinity",
+            "palemoon": "25",
+            "camino": "Infinity",
+            "maxthon": "4.5",
+            "seamonkey": "2.3",
+            "theme": "blue"
+        }
+    });
+    /*Конец документа*/
 });
